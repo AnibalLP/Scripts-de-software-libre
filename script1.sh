ece77fb7fd8404522ca692a81f993b8cf7d7bd7f
Anibal Portillo <48638366+AnibalLP@users.noreply.github.com>
2021-09-11 11:03:59 -0600

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+groupadd familia
+adduser maria
+adduser anibal
+adduser maria familia
+adduser anibal familia
+cat /etc/group/
+echo "has creado un grupo y dos usuario"
+echo "adios cuidate amig@"
+exit
